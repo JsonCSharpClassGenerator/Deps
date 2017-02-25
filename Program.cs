40f31f2e9a1cb2560b762ef9ce5e52a371934ab1
Tong Sun <suntong@cpan.org>
2017-02-25 12:24:29 -0500

    - [+] Initial commit with Newtonsoft.Json & Scriban

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Scriban;
+
+namespace Deps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
